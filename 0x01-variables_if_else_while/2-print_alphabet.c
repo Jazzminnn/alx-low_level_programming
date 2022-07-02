08e2cde1fd480d1df4fbc76cc5046cd24c98585c
jazzminnn <jasmineenyo@gmail.com>
2022-07-02 16:52:24 -0700

    stress

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-entry point
+*description:print alphabets in lowercase /n
+*Return:Always 0
+*/
+
+int main(void)
+{
+	char n = 'a';
+
+for (n = 'a'; n <= 'z'; n++)
+{
+	putchar(n);
+
+putchar('\n');
+}
+return (0);
+
+}
