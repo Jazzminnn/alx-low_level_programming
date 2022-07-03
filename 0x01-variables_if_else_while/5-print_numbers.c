f11ece1e6f40a7a536881c1179b182686c39b462
jazzminnn <jasmineenyo@gmail.com>
2022-07-03 09:03:18 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main-prints number 0 to 9 base10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar('\n');
+
+return (0);
+
+
+}
