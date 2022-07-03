65c98a89ec1d0ab2989c18413401f6cfb7a1a17f
jazzminnn <jasmineenyo@gmail.com>
2022-07-03 08:26:25 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main- prints alphabets without printing q,e
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char n = 'a';
+
+	for (n = 'a'; n <= 'z'; n++)
+	if (n != 'q' && n != 'e')
+	putchar(n);
+		
+putchar('\n');
+
+return (0);
+
+}
